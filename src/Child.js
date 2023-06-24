de3fd742a8dea581717a71d4cebd41e0c308ba35
heexohee <rsoy2918@naver.com>
2023-06-24 20:07:49 +0900

    구조분해할당 장점

--- src/Child.js
+++ src/Child.js
@@ -1,5 +1,7 @@
 import React from 'react'
-//child안에서 props를 하나 받을 수 있다. 
+//child안에서 props를 받을 수 있다. 
+//구조 분해 할당한 모습 -> 이렇게 쓰면 어떤 Child, 어떤 props가 
+// 있는지를 명시적으로 알 수 있다.
 function Child({age, name, children}) {
   console.log(age);
   console.log(name);
