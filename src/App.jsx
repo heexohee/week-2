e7ff2f04954245e9afdbc9bd569275953c8072b7
heexohee <rsoy2918@naver.com>
2023-06-26 11:31:06 +0900

    usestate예시1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Child from 'Child'//import해줘야 함!
 
 function App() {
-  const name='test';
-
-  return <Child 
-  age={21} 
-  name={name}>
-    이름
-    </Child>;
-    //현재 props가 세개!-> App.js에서 받을 것
   
+
+  // hook
+  // 구조 분해 할당으로
+  // const [state, useState] = useState('initial Value');
+ 
+  //const [count, setCount] = useState(0); 
+  //const [todoList, setTodoList] = useState([]);
+
+  const [name, setName] = useState("김할아부지");
+
+
+  return (<div>
+   {name}
+   <br/>
+   <button 
+   onClick = {function(){
+   alert("hello");
+  }}
+   >
+    클릭</button>
+   </div>)
 }
 
 export default App
