df21f2856f22bb21f1815ac2f062aaf84512ca14
heexohee <rsoy2918@naver.com>
2023-06-26 12:06:06 +0900

    리액트 불변성 지키기 예시

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import Child from 'Child'//import해줘야 함!
+// import React, { useState } from 'react'
+// import Child from 'Child'//import해줘야 함!
 
-function App() {
+// function App() {
   
 
   // hook
@@ -17,19 +17,23 @@ function App() {
   // onChange 속성에는 항상 event가 매개변수로 딸려온다!
 // 우리가 입력하는 값들이 매개변수로 event.target.value에 들어온다!
 //input 태그와 state와 값을 동기화 했다!  
-const [fruit, setFruit]=useState('');
+import React, { useState } from "react";
+
+function App() {
+  const [dogs, setDogs] = useState(["말티즈"]);
+
+  function onClickHandler() {
+		// spread operator(전개 연산자)를 이용해서 dogs를 복사합니다. 
+	  // 그리고 나서 항목을 추가합니다.
+    setDogs([...dogs, "시고르자브르종"]);
+  }
+
+  console.log(dogs);
   return (
-  <div>
-   과일 : <input 
-   value={fruit} 
-   onChange={function(event){
-    //console.log("event", event.target.value);
-   setFruit(event.target.value);
-   }}/>
-   <br/> <br/>
-  
-   </div>)
+    <div>
+      <button onClick={onClickHandler}>버튼</button>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
